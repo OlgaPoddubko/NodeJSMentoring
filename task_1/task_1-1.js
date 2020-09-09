8c4b334b39f635060f40431716a81a36ff45dc6e
Volha_Paddubka <volha_paddubka@epam.com>
2020-09-09 20:25:54 +0300

    task_1/clear branch

--- task_1/task_1-1.js
+++
@@ -1,7 +0,0 @@
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  while (null !== (chunk = process.stdin.read())) {
-    const reversedString = Array.from(chunk).reverse().join('')
-    process.stdout.write(`${reversedString}\n`);
-  }
-});
